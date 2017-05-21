0fc4f19359f38a0b700f6994001da41e549019e8
Mariano Converti <mariano.converti@southworks.com>
2017-05-21 00:50:13 -0300

    Add sample web client for 202-listing-and-subclipping-apis functions

---
+++ 202-listing-and-subclipping-apis/web-client/server.js
@@ -0,0 +1,20 @@
+// jshint esversion:6
+const restify = require('restify');
+const path = require('path');
+const server = restify.createServer();
+
+server.get('/config.json', (req, res, next) => {
+    var config = {
+        baseUrl: process.env.API_BASE_URL
+    };
+    res.json(200, config);
+});
+
+server.get(/\/?.*/, restify.serveStatic({
+    directory: path.join(__dirname, 'static'),
+    default: 'index.html'
+}));
+
+server.listen(process.env.PORT || 8080, () => {
+    console.log("server up");
+});
